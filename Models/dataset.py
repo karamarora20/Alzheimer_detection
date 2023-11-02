8a048d00521ecb0be51324069b97c76a45dd80b9
Karam <Karam.arora20@st.niituniversity.in>
2023-11-02 12:14:22 +0530

    created file for future use

---
+++ Models/dataset.py
@@ -0,0 +1,48 @@
+
+def load_train_data():
+    """
+    This function loads a dataset of images from a specified path, resizes the images, 
+    and stores them in a pandas DataFrame along with their respective classes.
+
+    The images are categorized into four classes: 'MildDemented', 'ModerateDemented', 
+    'NonDemented', and 'VeryMildDemented'. The function iterates through each class, 
+    opens and resizes the images, and appends them to a dictionary along with their class.
+
+    The dictionary is then converted into a pandas DataFrame and returned.
+
+    Returns:
+        df_train (pd.DataFrame): A DataFrame containing the resized images and their classes.
+    """
+    
+    df_train={'class':[],'image':[]} # create a dictionary to store the dataset
+
+    """# create dataset from the image folder"""
+
+    classes=['MildDemented','ModerateDemented','NonDemented','VeryMildDemented']
+    train_path=''
+
+    for i in classes:
+        if i=='MildDemented':
+            train_path='../Dataset/train/MildDemented/mildDem'
+        elif i=='ModerateDemented':
+            train_path='../Dataset/train/ModerateDemented/moderateDem'
+        elif i=='NonDemented':
+            train_path='../Dataset/train/NonDemented/nonDem'
+        else:
+            train_path='../Dataset/train/VeryMildDemented/veryMildDem'
+        for j in range(100000):
+            try:
+                img=Image.open(f'{train_path}{j}.jpg')
+                df_train['class'].append(i)
+                df_train['image'].append(np.array(img.resize((200,200))).reshape(200*200,1))
+
+            except:
+                break
+
+    """# save Dataset into a pandas Dataframe"""
+    return pd.DataFrame(df_train)
+def load_test_data():
+    pass
+# df=load_train_data()
+# print(df.head())
+
